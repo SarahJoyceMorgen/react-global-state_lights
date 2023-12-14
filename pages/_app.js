846aa7f49dae0439e4ac3daa29195dd1e0790c8b
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-14 11:45:18 +0100

    toggleLight Update

--- pages/_app.js
+++ pages/_app.js
@@ -4,24 +4,30 @@ import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
 
-    const [lights, setLights] = useState([
-      { id: "1", name: "livingroom", isOn: false },
-      { id: "2", name: "kitchen", isOn: false },
-      { id: "3", name: "bedroom", isOn: false },
-      { id: "4", name: "bathroom", isOn: false },
-      { id: "5", name: "garage", isOn: false },
-      { id: "6", name: "porch", isOn: false },
-      { id: "7", name: "garden", isOn: false },
-      { id: "8", name: "office", isOn: false },
-      
-    
-    ]);
+  const [lights, setLights] = useState([
+    { id: "1", name: "livingroom", isOn: false },
+    { id: "2", name: "kitchen", isOn: false },
+    { id: "3", name: "bedroom", isOn: false },
+    { id: "4", name: "bathroom", isOn: false },
+    { id: "5", name: "garage", isOn: false },
+    { id: "6", name: "porch", isOn: false },
+    { id: "7", name: "garden", isOn: false },
+    { id: "8", name: "office", isOn: false },
 
 
-  return (
-    <Layout>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </Layout>
+  ]);
+
+  const toggleLight = (id) => {
+    setLights((lights) => lights.map((light) =>
+  light.id === id ? { ...lights, isOn: !lights.isOn } : light
+  )
   );
+}
+
+return (
+  <Layout>
+    <GlobalStyle />
+    <Component {...pageProps} />
+  </Layout>
+);
 }

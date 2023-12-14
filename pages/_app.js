8088d474fa05bc6a999a9ef31698cfa9bdf7889f
Sarah Morgen <sarah.morgen@outlook.de>
2023-12-14 11:33:32 +0100

    array

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+
+    const [lights, setLights] = useState([
+      { id: "1", name: "livingroom", isOn: false },
+      { id: "2", name: "kitchen", isOn: false },
+      { id: "3", name: "bedroom", isOn: false },
+      { id: "4", name: "bathroom", isOn: false },
+      { id: "5", name: "garage", isOn: false },
+      { id: "6", name: "porch", isOn: false },
+      { id: "7", name: "garden", isOn: false },
+      { id: "8", name: "office", isOn: false },
+      
+    
+    ]);
+
+
   return (
     <Layout>
       <GlobalStyle />
